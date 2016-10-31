9f9e706a0d3249a51772b643bd5b972be8ce443c
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-10-31 19:56:46 +0100

    Update external.js

--- JS-Training/example3/external.js
+++ JS-Training/example3/external.js
@@ -2,10 +2,8 @@
 		/*
 		# This video course is  exclusively available in Udemy
 		*/
-    
 			var a = 50; //DEClARING AND INITIAlIZING VARIABlE A
 			var b = 60; //declaring and initializing variable b
 			var sum = a+b; // adding values of a and b in the  variable sum
 			document.write(sum); // printing the values of sum
-
 //</script>
